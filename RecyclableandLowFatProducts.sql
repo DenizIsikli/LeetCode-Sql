4bf90204e673eb0d4b57cff6db43ceb19238ae80
DenizIsikli <s215818@dtu.dk>
2023-01-26 21:20:33 +0100

    Added comments

--- RecyclableandLowFatProducts.sql
+++ RecyclableandLowFatProducts.sql
@@ -1,4 +1,5 @@
 SELECT product_id FROM Products
 WHERE low_fats = 'Y' AND recyclable = 'Y'
 
--- I simply select 
+-- I simply select the "produdt_id" from "Products"
+-- where I check whether both "low_fats" and "recyclable" have "Y" in them
