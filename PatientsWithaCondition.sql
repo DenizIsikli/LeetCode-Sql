dd07bc97eca45bcf8be88da2dc8295a6f8510cb1
DenizIsikli <s215818@dtu.dk>
2023-01-27 23:17:02 +0100

    PatientsWithaCondition

---
+++ PatientsWithaCondition.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT * FROM Patients
+WHERE conditions like '% DIAB1%' OR conditions like 'DIAB1%'
+
+-- I select all from "Patiens" where "conditions" is either "DIAB1" 
+-- with something behind it or in front of it
