8d5e72ed57603b4cf9aaee5121a8093ee7bacf9b
DenizIsikli <s215818@dtu.dk>
2023-01-30 00:01:30 +0100

    Minor changes

--- CombineTwoTables.sql
+++ CombineTwoTables.sql
@@ -1,3 +1,8 @@
 SELECT firstName, lastName, city, state 
 FROM Person LEFT JOIN Address
 ON Person.personId = Address.personId
+
+-- I select "firstname", "lastname", "city", and "state" from "Person" where I 
+-- use "LEFT JOIN" on the "Adress" table and the "ON" function where I specify 
+-- a join condition which I do on the last line: 
+-- On the condition that "Person.personId" equals "Address.personId" 
