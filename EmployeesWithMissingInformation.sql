b1d16b284ec95359c9000645b6ebf1d58ee80ae2
DenizIsikli <s215818@dtu.dk>
2023-01-29 23:23:21 +0100

    EmployeesWithMissingInformation

---
+++ EmployeesWithMissingInformation.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+SELECT employee_id FROM Employees
+WHERE employee_id NOT IN (SELECT employee_id FROM Salaries)
+UNION -- Union is used to combine the result-set of two or more SELECT statements.
+SELECT employee_id FROM Salaries
+WHERE employee_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY employee_id ASC
+
+-- I select "employee_id" from "Employees" where I check if 
+-- "employee_id" is not in "employee_id" from the "Salaries" table
+-- Then I do the same but starting from the "Salaries" table, 
+-- where I again check if "employee_id" is not in "employee_id" 
+-- from the "Employees table"
+
+-- Basically checking both ways possible, and then
+-- use "Union" inbetween to combine them
+
+-- At last I order by "employee_id" in an ascending order
