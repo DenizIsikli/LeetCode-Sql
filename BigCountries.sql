1f81f8140caa593ab877092e628d34467dd0fc84
DenizIsikli <s215818@dtu.dk>
2023-01-30 00:04:42 +0100

    Minor changes

--- BigCountries.sql
+++ BigCountries.sql
@@ -2,3 +2,7 @@ SELECT name, population, area
 FROM World
 WHERE area >= 3000000 
 OR population >= 25000000
+
+-- I select "name", "population", and "area" from "World" where "area"
+-- is above or equal to 3000000 or where "population" is above or equal
+-- to 25000000
--- CustomersWhoNeverOrder.sql
+++ CustomersWhoNeverOrder.sql
@@ -1,2 +1,5 @@
 SELECT name AS Customers FROM Customers 
 WHERE id NOT IN (SELECT customerId FROM Orders)
+
+-- I select "name" as "Customers" from "Customers" where "id" is 
+-- not in "customerId" from the "Orders" table
