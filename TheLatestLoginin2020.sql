47fc65bc8830aebbfd8a899308195765cd2d6eb5
DenizIsikli <s215818@dtu.dk>
2023-02-01 22:00:25 +0100

    TheLatestLoginin2020

---
+++ TheLatestLoginin2020.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT DISTINCT user_id, MAX(time_stamp) AS last_stamp
+FROM Logins
+WHERE YEAR(time_stamp) = 2020 
+GROUP BY user_id
+
+-- I distinctively select "user_id" and the maximum of "time_stamp" as 
+-- "last_stamp" from the "Logins" table where I then group by "user_id"
