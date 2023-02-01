cce076dc3d5e139ddfaf63c7e7973c11ae6dafa7
DenizIsikli <s215818@dtu.dk>
2023-02-01 21:59:42 +0100

    FindTotalTimeSpentbyEachEmployee

---
+++ FindTotalTimeSpentbyEachEmployee.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT DISTINCT event_day AS day, emp_id, SUM(out_time - in_time) AS total_time
+FROM Employees
+GROUP BY event_day, emp_id
+
+-- I distinctively select "event_day", "emp_id", and the sum of
+-- "out_time" minus "in_time" as "total_time" from the "Employees"
+-- table, where I then group by "event_day" and "emp_id"
