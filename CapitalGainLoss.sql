bdf73756b53311274cfa3ccc4719b233835627da
DenizIsikli <s215818@dtu.dk>
2023-02-02 23:47:36 +0100

    CapitalGainLoss

---
+++ CapitalGainLoss.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT stock_name, SUM(IF(operation='sell', price,-price)) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name
+
+-- I firstly select "stock_name", and then I sum "price" with 
+-- minus "price" only if "operation" equals  to "sell" from the 
+-- "Stocks" table
+
+-- At last grouped by "stock_name"
