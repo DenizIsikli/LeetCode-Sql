ca6ec002e38627f63da4b4b7d459b4e06f29cbf0
DenizIsikli <s215818@dtu.dk>
2023-02-04 00:29:05 +0100

    SalesAnalysisIII

---
+++ SalesAnalysisIII.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT P.product_id, P.product_name
+FROM Product P
+LEFT JOIN Sales S
+ON P.product_id = S.product_id
+GROUP BY P.product_id
+HAVING SUM(S.sale_date BETWEEN '2019-01-01' and '2019-03-31') = COUNT(S.sale_date)
+
+-- I select "P.product_id" and "P.product_name" from the 
+-- "Product" table as "P" left joining the "Sales" table as "S"
+-- on the condition that "P.product_id" equals "S.product_id"
+
+-- I then group by "P.product_id" having the sum of "S.sale_date" 
+-- between the given dates, equal to the count of "S.sale_date" 
