370c9000cd3b6dc8a58f092a985316babfb0f965
DenizIsikli <s215818@dtu.dk>
2023-02-04 00:02:40 +0100

    BankAccountSummaryII

---
+++ BankAccountSummaryII.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT U.name AS NAME,
+SUM(T.amount) AS BALANCE
+FROM Transactions T 
+LEFT JOIN Users U 
+ON U.account = T.account
+GROUP BY U.account
+HAVING SUM(T.amount) > 10000
+
+-- I select "U.name" as "NAME", and sum "T.amount" AS "BALANCE" 
+-- from the "Transactions" table as "T" left joining the "Users" 
+-- table as U
+
+-- At last I group by U.account having the balance to be over 10000
