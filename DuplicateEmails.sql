696e2944823af6fb363113e63b047ef06f55a94b
DenizIsikli <s215818@dtu.dk>
2023-02-05 23:55:23 +0100

    Minor changes

--- DuplicateEmails.sql
+++ DuplicateEmails.sql
@@ -3,5 +3,5 @@ FROM Person GROUP BY Email
 HAVING COUNT(*)>1
 
 -- I select "email" as "Email" from "Person" and group by "Email"
--- where I use the HAVING() because the WHERE keyword cannot be used 
--- with aggregate functions.
+-- where I use the HAVING() because the WHERE keyword cannot be 
+-- used with aggregate functions.
