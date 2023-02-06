8b9ca547720a26501a454f9fbbf893bdf63ff10b
DenizIsikli <s215818@dtu.dk>
2023-02-06 23:37:30 +0100

    Minor changes

--- PatientsWithaCondition.sql
+++ PatientsWithaCondition.sql
@@ -2,5 +2,5 @@
 SELECT * FROM Patients
 WHERE conditions like '% DIAB1%' OR conditions like 'DIAB1%'
 
--- I select all from "Patiens" where "conditions" is either "DIAB1" 
--- with something behind it or in front of it
+-- I select all from "Patiens" where "conditions" is either 
+-- "DIAB1" with something behind it or in front of it
