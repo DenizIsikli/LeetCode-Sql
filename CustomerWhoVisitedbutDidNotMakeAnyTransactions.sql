3bb411d451783646fb49e5f3b8fddb6d0148741f
DenizIsikli <s215818@dtu.dk>
2023-02-07 23:55:31 +0100

    Minor changes

--- CustomerWhoVisitedbutDidNotMakeAnyTransactions.sql
+++ CustomerWhoVisitedbutDidNotMakeAnyTransactions.sql
@@ -7,8 +7,8 @@ WHERE t.visit_id IS NULL
 GROUP BY customer_id
 
 -- I select "customer_id" from "Visits" as "v", where I also count "visit_id" from "Visits"
--- I then use "LEFT JOIN" on the "Transactions" table as "t" and the "ON" function where 
--- I specify a join condition which I do on line 5:
+-- I then use "LEFT JOIN" on the "Transactions" table as "t" and the "ON" function 
+-- where I specify a join condition which I do on line 5:
 -- On the condition that "v.visit_id" is equal to 
 -- "t.visit_id" where "t.visit_id" is null
 
