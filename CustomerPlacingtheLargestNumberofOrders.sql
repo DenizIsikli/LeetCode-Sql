098bc2d5a0dc5cfee73d093c278877ea538fa503
DenizIsikli <s215818@dtu.dk>
2023-02-11 23:50:44 +0100

    Minor changes

--- CustomerPlacingtheLargestNumberofOrders.sql
+++ CustomerPlacingtheLargestNumberofOrders.sql
@@ -4,5 +4,5 @@ ORDER BY COUNT(DISTINCT order_number) DESC
 LIMIT 1
 
 -- I select "customer_number" from "Orders" and group them by "customer_number"
--- where I then order by the count, distinct of "order_number" in a descending order 
--- and put the limit to 1 (only showcasing a single one)
+-- where I then order by the count, distinct of "order_number" in a descending 
+-- order  and put the limit to 1 (only showcasing a single one)
