b906ea75de347daa36dc74e8bde117e5ee587d03
Bobby <s215818@dtu.dk>
2023-03-03 22:00:00 +0100

    Minor changes

--- CalculateSpecialBonus.sql
+++ CalculateSpecialBonus.sql
@@ -6,6 +6,7 @@ ORDER BY employee_id ASC
 
 -- I select "employee_id" from "Employees" if "employee_id" is not divisble by 2
 -- and if the first letter of the name is not "M"
+
 -- I then put the "salary" to 0 as "bonus"
 
 -- Lastly I order by "employee_id" in an ascending order
