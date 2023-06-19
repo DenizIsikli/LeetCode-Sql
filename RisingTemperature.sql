b992263833535b827e50ac5a156f933d8da6071c
Bobby <s215818@dtu.dk>
2023-06-19 22:47:55 +0200

    Minor changes

--- RisingTemperature.sql
+++ RisingTemperature.sql
@@ -5,6 +5,6 @@ WHERE w1.temperature > w2.temperature
 AND DATEDIFF(w1.recordDate, w2.recordDate) = 1
 
 -- I create two objects of the "Weather" table, where I select "id" from 
--- the "w1" object if "w1.temperature" is bigger than "w2.temperature" 
--- and if the difference between the dates if 1
--- I check this by using the "DATEDIFF" function to compare dates
+-- the "w1" object where "w1.temperature" is bigger than "w2.temperature" 
+-- and where the difference between the dates equal to 1 which is done
+-- by using the "DATEDIFF" function to compare dates
