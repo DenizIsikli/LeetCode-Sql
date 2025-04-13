ec49e7431fd7a8b283501b6e4e49dab1870678d5
Deniz <s215818@dtu.dk>
2025-04-13 23:06:50 +0200

    DNAPatternRecognition.sql

---
+++ Problemsets/DNAPatternRecognition.sql
@@ -0,0 +1,15 @@
+SELECT *,
+(CASE
+WHEN dna_sequence like 'ATG%' THEN 1
+ELSE 0 END) AS has_start,
+(CASE
+WHEN dna_sequence like '%TAA' OR dna_sequence like '%TAG' OR dna_sequence like '%TGA' THEN 1
+ELSE 0 END) AS has_stop,
+(CASE
+WHEN dna_sequence like '%ATAT%' THEN 1
+ELSE 0 END) AS has_atat,
+(CASE
+WHEN dna_sequence like '%GGG%' THEN 1
+ELSE 0 END) AS has_ggg
+FROM Samples
+ORDER BY sample_id;
